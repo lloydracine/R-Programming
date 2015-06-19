097a2fa25cf6c87fdec0388767376bc3b766e00e
lloydracine <lloyd@lsventures.com>
2015-06-19 10:50:08 -0500

    Comments added

--- rankall.R
+++ rankall.R
@@ -1,6 +1,35 @@
+## Programming Assignment 3
+##
+## The function reads the outcome-of-care-measures.csv file and returns a 
+## a 2-column data frame containing the hospital in each state that has the 
+## ranking specified in num.
+## 
+## If the number given by num is larger than the number of hospitals in that
+## state, then the function should return NA. Hospitals that do not have data 
+## on a particular outcome should be excluded from the set of hospitals when 
+## deciding the rankings.
+##
+## The first column in the data frame is named hospital, which contains the 
+## hospital name, and the second column is named state, which contains the 
+## 2-character abbreviation for the state name. Hospitals that do not have 
+## data on a particular outcome should be excluded from the set of hospitals 
+## when deciding the rankings. 
+
 rankall <- function(outcome, num = "best") {
-    
-    ## Reads outcome data
+  ## 'outcome' Outcome name.  Can be one of "heart attack", "heart failure", 
+  ## or "pneumonia".    If an invalid outcome value is passed to the function 
+  ## should throw an error via the stop function with the exact message 
+  ## "invalid outcome".
+  
+  ## 'num' the ranking of a hospital in the state for the outcome. Can take 
+  ## values "best", "worst", or an integer indicating the ranking (smaller 
+  ## numbers are better).
+  
+  ## returnS a data frame containing the names of the hospitals that are the 
+  ## best in their respective states for 30-day heart attack death rates. 
+  ## The function should return a value for every state (some may be NA).
+  
+  ## Reads outcome data
     file_data <- read.csv("outcome-of-care-measures.csv", sep = ",")
     
     ## Checks that state and outcome are valid
